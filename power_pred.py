dc3307323a2e8f3fc98219751cedee7c3f53970d
json9101 <sjunsung981@gmail.com>
2023-08-17 10:38:52 +0900

    random forest features 변화

--- power_pred.py
+++ power_pred.py
@@ -42,71 +42,27 @@ merged_df.dropna(subset=['습도(%)'], inplace=True)
 
 merged_df=pd.get_dummies(merged_df, columns=['건물유형'])
 
-features =['기온(C)','강수량(mm)','풍속(m/s)','습도(%)','연도','월','일','시']
-target = "일조(hr)"
-
-labeled_data = merged_df[merged_df[target].notnull()] 
-unlabeled_data = merged_df[merged_df[target].isnull()]
-
-X_labeled = labeled_data[features]
-y_labeled = labeled_data[target]
-X_unlabeled = unlabeled_data[features]
+x = merged_df[['기온(C)','강수량(mm)','습도(%)','연도','월','일','시','연면적(m2)','건물유형_건물기타',
+       '건물유형_공공', '건물유형_대학교', '건물유형_데이터센터', '건물유형_백화점및아울렛', '건물유형_병원',
+       '건물유형_상용', '건물유형_아파트', '건물유형_연구소', '건물유형_지식산업센터', '건물유형_할인마트',
+       '건물유형_호텔및리조트']]
+y = merged_df[['전력소비량(kWh)']]
 
 
-clf = TabNetRegressor()
-clf.fit(
-    X_labeled.values, y_labeled.values.reshape(-1,1),
-    eval_set=[(X_labeled.values, y_labeled.values.reshape(-1,1))]
-)
+scaler = StandardScaler()
 
-unlabeled_predictions = clf.predict(X_unlabeled.values)
-merged_df.loc[merged_df[target].isnull(), target] = unlabeled_predictions
+ranfor = RandomForestRegressor()
+X = scaler.fit_transform(x)
 
-cleaned_merged_df1 = merged_df.copy()
-
-features1 =['기온(C)','풍속(m/s)','강수량(mm)','습도(%)','연도','월','일','시', "일조(hr)"]
-target1 = "일사(MJ/m2)"
-
-labeled_data1 = cleaned_merged_df1[cleaned_merged_df1[target1].notnull()] 
-unlabeled_data1 = cleaned_merged_df1[cleaned_merged_df1[target1].isnull()]
-
-X_labeled1 = labeled_data1[features1]
-y_labeled1 = labeled_data1[target1]
-X_unlabeled1 = unlabeled_data1[features1]
-
-clf1 = TabNetRegressor()
-clf1.fit(
-    X_labeled1.values, y_labeled1.values.reshape(-1,1),
-    eval_set=[(X_labeled1.values, y_labeled1.values.reshape(-1,1))]
-)
-
-unlabeled_predictions1 = clf1.predict(X_unlabeled1.values)
-
-cleaned_merged_df1.loc[cleaned_merged_df1[target1].isnull(), target1] = unlabeled_predictions1
-
-cleaned_merged_df1['일사(MJ/m2)'] = cleaned_merged_df1['일사(MJ/m2)'].apply(lambda x: max(x, 0))
-
-X = cleaned_merged_df1[['습도(%)','연도','월','일','시','연면적(m2)','건물유형_건물기타', '건물유형_공공', '건물유형_대학교', '건물유형_데이터센터', '건물유형_백화점및아울렛',
-       '건물유형_병원', '건물유형_상용', '건물유형_아파트', '건물유형_연구소', '건물유형_지식산업센터',
-       '건물유형_할인마트', '건물유형_호텔및리조트']]
-y = cleaned_merged_df1[['전력소비량(kWh)']]
-
-scaler1 = StandardScaler()
-
-ranfor_add = RandomForestRegressor()
-X = scaler1.fit_transform(X)
-
-X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.3,random_state=5)
+X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.3,random_state=8)
 
 y_train_flat = y_train.values.reshape(-1)
 
-ranfor_add.fit(X_train,y_train_flat)
+ranfor.fit(X_train,y_train_flat)
 
-ran_y_pred1 = ranfor_add.predict(X_test)
+ran_y_pred = ranfor.predict(X_test)
 
-ran_mape1 =mean_absolute_percentage_error(y_test,ran_y_pred1)
+ran_mape =mean_absolute_percentage_error(y_test,ran_y_pred)
 
-print(ran_mape1)
+print(ran_mape)
 
-with open('rf_model.pkl', 'wb') as file:
-    pickle.dump(ranfor_add, file)
